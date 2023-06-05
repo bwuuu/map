17387fa8b5e8a4a2063cced440676a6ba8d90184
bwuuu <117622597+bwuuu@users.noreply.github.com>
2023-06-05 17:17:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Data
 data = {
@@ -15,17 +14,9 @@ df = pd.DataFrame(data)
 # Header
 st.header('Volume of Each District in Taipei City')
 
-# Heatmap
-st.subheader('Heatmap')
-fig, ax = plt.subplots()
-heatmap = ax.imshow(df['Count'].values.reshape(1, -1), cmap='YlGnBu')
-ax.set_xticks(range(len(df['District'])))
-ax.set_xticklabels(df['District'], rotation=90)
-ax.set_yticks([])
-ax.set_xlabel('District')
-ax.set_ylabel('Count')
-fig.colorbar(heatmap)
-st.pyplot(fig)
+# Map
+st.subheader('Map')
+st.map(df)
 
 # Table
 st.subheader('Table')
