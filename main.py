780950a66de0bcfb3f6e3d693e56250e17488a7a
bwuuu <117622597+bwuuu@users.noreply.github.com>
2023-06-05 17:15:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Data
 data = {
@@ -17,8 +17,15 @@ st.header('Volume of Each District in Taipei City')
 
 # Heatmap
 st.subheader('Heatmap')
-heatmap_fig = sns.heatmap(data=df, x='District', y='Count', annot=True, cmap='YlGnBu', fmt='d')
-st.pyplot(heatmap_fig.figure)
+fig, ax = plt.subplots()
+heatmap = ax.imshow(df['Count'].values.reshape(1, -1), cmap='YlGnBu')
+ax.set_xticks(range(len(df['District'])))
+ax.set_xticklabels(df['District'], rotation=90)
+ax.set_yticks([])
+ax.set_xlabel('District')
+ax.set_ylabel('Count')
+fig.colorbar(heatmap)
+st.pyplot(fig)
 
 # Table
 st.subheader('Table')
